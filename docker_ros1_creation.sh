e9ba11fc99f738abdd9d3b3c6dd9a5aa27fdf022
koala <jonathan.cacace@gmail.com>
2023-02-14 12:13:41 +0100

    consider the username in the docker creation

--- docker_ros1_creation.sh
+++ docker_ros1_creation.sh
@@ -5,7 +5,7 @@ xhost +
 ##Without sharing a folder
 docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb \
 --env=LOCAL_USER_ID="$(id -u)" \
--v /home/jcacace/dev:/home/dev/:rw \
+-v ~/dev:/home/dev/:rw \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -e DISPLAY=:0 \
 --network host \
